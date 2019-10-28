cd3b1cd8c867f6523b32949e7e1fc9bf174e9192
Victor Luã <56847759+victor212luan@users.noreply.github.com>
2019-10-28 19:04:44 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/victor/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,12 @@
+package com.victor.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.victor.cursomc.domain.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+	
+}
